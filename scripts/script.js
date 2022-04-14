0ed0ff65b98d42d1be15a621ad6b507648a8fe76
Sergey Popkov <sadi213@yandex.ru>
2022-04-14 23:38:24 +0300

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+// alert("salam!");
+// let result = confirm("are you ezid?");
+// console.log(result);
+// if(result){
+//     alert("les goy to the lounge!) brat");
+// }else{
+//     alert("fuck you retard russian pork shwaine!!!");
+// }
+// let answer = prompt("are you ezid?", "yes I am Ezid brat!!!");
+let a=1, b=2, c=3;
+
